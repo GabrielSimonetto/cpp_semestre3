ba741e10e6301be4a0d3b7fa9f9ec2c5fe06980e
gabriel <gabrielfranksimonetto@gmail.com>
2019-04-14 15:31:26 -0300

    array_list.h

--- ED/array_list/array_list.h
+++ ED/array_list/array_list.h
@@ -29,8 +29,8 @@ public:
     std::size_t size() const;
     std::size_t max_size() const;
     T& at(std::size_t index);
-    T& operator[](std::size_t index);
-    const T& at(std::size_t index) const;
+    T& operator[](std::size_t index); // nao verifica o endereço
+    const T& at(std::size_t index) const; // verifica se voce TEM aquele elemento
     const T& operator[](std::size_t index) const;
 
 private:
@@ -44,3 +44,178 @@ private:
 }
 
 #endif
+
+template<typename T>
+structures::ArrayList<T>::ArrayList() {
+    max_size_ = DEFAULT_MAX;
+    contents = new T[max_size_];
+    size_ = 0;
+}
+
+template<typename T>
+structures::ArrayList<T>::ArrayList(std::size_t max_size) {
+    max_size_ = max_size;
+    contents = new T[max_size_];
+    size_ = 0;
+}
+
+template<typename T>
+structures::ArrayList<T>::~ArrayList() {
+    delete [] contents;
+}
+
+template<typename T>
+void structures::ArrayList<T>::clear() {
+    size_ = 0
+}
+
+template<typename T>
+void structures::ArrayList<T>::push_back(const T& data) {
+    if (full()) {
+        throw std::out_of_range("lista cheia");
+    } else {
+        contents[size_] = data;
+        size_++;   
+    }
+}
+
+template<typename T>
+void structures::ArrayList<T>::push_front(const T& data) {
+    insert(data ,0)
+}
+
+template<typename T>
+void structures::ArrayList<T>::insert(const T& data, std::size_t index) {
+    if (full()) {
+        throw std::out_of_range("lista cheia");        
+    } else {
+        size_++;
+        for (int i = size_; i >= index; i--) {
+            contents[i] = contents[i-1];
+        }
+    }
+}
+
+template<typename T>
+void structures::ArrayList<T>::insert_sorted(const T& data) {
+    if (full()) {
+        throw std::out_of_range("lista cheia");        
+    } else {
+        int i = 0;
+        while (contents[i] < data) {
+            i++;
+        }
+        insert(i);
+    }
+}
+
+template<typename T>
+T structures::ArrayList<T>::pop(std::size_t index) {
+    if (empty()) {
+        throw std::out_of_range("lista vazia");
+    } else {
+        for (int i = index+1; i < size_; i++) {
+            contents[i-1] = contents[i];
+        }        
+    }
+}
+
+template<typename T>
+T structures::ArrayList<T>::pop_back() {
+    if (empty()) {
+        throw std::out_of_range("lista vazia");
+    } else {
+        size_--;  
+    }
+}
+
+template<typename T>
+T structures::ArrayList<T>::pop_front() {
+    if (empty()) {
+        throw std::out_of_range("lista vazia");
+    } else {
+        pop(0);
+    }
+}
+
+template<typename T>
+void structures::ArrayList<T>::remove(const T& data) {
+    if (empty()) {
+        throw std::out_of_range("lista vazia");
+    } else {
+        for (int i = 0; i < size_; i++) {
+            if (contents[i] == data) {
+                pop(i)
+                i--;
+            }
+        }
+    }
+}
+
+template<typename T>
+bool structures::ArrayList<T>::full() const{
+    return size_ >= max_size_;
+}
+
+template<typename T>
+bool structures::ArrayList<T>::empty() const{
+    return size_ == 0;
+}
+
+template<typename T>
+bool structures::ArrayList<T>::contains(const T& data) const {
+    for (int i = 0; i < size_; i++) {
+        if (contents[i] == data) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template<typename T>
+std::size_t structures::ArrayList<T>::size() const {
+    return size_;
+}
+
+template<typename T>
+std::size_t structures::ArrayList<T>::max_size() const {
+    return max_size_;
+}
+
+template<typename T>
+std::size_t structures::ArrayList<T>::find(const T& data) const {
+    for (int i = 0; i < size_; i++) {
+        if (contents[i] == data) {
+            return i;
+        }
+    }
+    return size_;
+}
+
+template<typename T>
+T& structures::ArrayList<T>::at(std::size_t index) {
+    if (index >= size_) {
+        throw std::out_of_range("esse elemento nao existe");
+    } else {
+        return contents[index];
+    }
+}
+
+template<typename T>
+T& structures::ArrayList<T>::operator [](std::size_t index) {
+    return contents[index];
+}
+
+template<typename T>
+const T& structures::ArrayList<T>::at(std::size_t index) const {
+    if (index >= size_) {
+        throw std::out_of_range("esse elemento nao existe");
+    } else {
+        return contents[index];
+    }
+}
+
+template<typename T>
+const T& structures::ArrayList<T>::operator [](std::size_t index) const {
+    return contents[index];
+}
